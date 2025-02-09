9f15bb33480770a17598e7ae058a3a7aedb5b769
harvind-here <harvindmukhal3@gmail.com>
2025-02-09 17:41:12 +0530

    redirect uri

--- managers/scheduling_manager.py
+++ managers/scheduling_manager.py
@@ -41,7 +41,7 @@ class SchedulingManager:
                     flow = Flow.from_client_secrets_file(
                         CREDENTIALS_FILE,
                         scopes=SCOPES,
-                        redirect_uri="http://localhost:5000/oauth_callback"
+                        redirect_uri=os.environ.get("GOOGLE_REDIRECT_URI")
                     )
                     auth_url, _ = flow.authorization_url(prompt='consent')
                     logger.info(f"Generated authorization URL: {auth_url}")
